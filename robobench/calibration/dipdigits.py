6c6f27b6855e0f33d1f33944b546f7c788356a7c
natagoh <natagoh@stanford.edu>
2017-07-13 12:20:46 -0700

    added test files

--- robobench/calibration/dipdigits.py
+++ robobench/calibration/dipdigits.py
@@ -3,6 +3,9 @@ import cv2
 from imutils import perspective
 from imutils import contours
 from skimage import exposure
+from skimage.filters import threshold_otsu, threshold_local
+from matplotlib import pyplot as plt
+import glob, os
 
 DIGITS_LOOKUP = {
     (1, 1, 1, 0, 1, 1, 1): 0,
@@ -35,7 +38,7 @@ def draw_rect(img, contour):
     # bounding rectangle
     rect = cv2.minAreaRect(contour)
     box = cv2.boxPoints(rect)
-    print("box points!!!:", box)
+    # print("box points!!!:", box)
     box = np.int0(box)
     img2 = img.copy()
     cv2.drawContours(img2,[box],0,(0,0,255),2)
@@ -81,12 +84,14 @@ def find_screen(img):
     cv2.imshow("bounding rect", temp)
     return pts
 
-def transform_img(img):
+def binarize_img(img):
     # make it gray in order to prepare it for threshold command
     img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     img_gray = exposure.rescale_intensity(img_gray, out_range = (0, 255))
-    cv2.imshow("transform gray", img_gray)
-    cv2.imshow("img", img)
+    # cv2.imshow("transform gray", img_gray)
+    # cv2.imshow("img", img)
+
+    # cv2.imshow("binary adaptive", binary_adaptive)
 
     # blur might not be necessary
     # blur = img_gray
@@ -94,18 +99,34 @@ def transform_img(img):
     # cv2.imshow("grayblur", blur)
 
     # make into binary image
-    ret,thresh = cv2.threshold(img_gray, 0, 255, cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
-    # kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (1, 5))
-    # thresh = cv2.morphologyEx(warped_thresh, cv2.MORPH_OPEN, kernel)
-    cv2.imshow("cleaned up", thresh)
-
+    ret,otsu_thresh = cv2.threshold(img_gray, 0, 255, cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
+    adapt_thresh = cv2.adaptiveThreshold(img_gray,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY_INV,11,2)
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT,(3,3))
+    opening = cv2.morphologyEx(adapt_thresh, cv2.MORPH_OPEN, kernel)
+    # cv2.imshow("cleaned up", adapt_thresh)
+    # cv2.imshow("cleaned up pt 2", opening)
 
     # dilate to join together segments
-    kernel = cv2.getStructuringElement(cv2.MORPH_RECT,(1,5))
-    # kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (1, 5))
-    dilation = cv2.dilate(thresh,kernel,iterations = 1)
-    cv2.imshow("dilated", dilation)
-    return dilation
+    kernel2 = cv2.getStructuringElement(cv2.MORPH_RECT,(1,5))
+    # kernel2 = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (1, 5))
+    otsu_dilated = cv2.dilate(otsu_thresh,kernel2,iterations = 1)
+    # cv2.imshow("dilated", otsu_dilated)
+
+    adapt_dilated = cv2.dilate(opening,kernel2,iterations = 1)
+    # cv2.imshow("dilated222", adapt_dilated)
+
+    titles = ['gray', 'adaptive thresholding','otsu thresh','opened adaptive img', 'dilated otsu', 'dilated adaptive']
+    images = [img_gray, adapt_thresh, otsu_thresh, opening, otsu_dilated, adapt_dilated]
+
+    """
+    for i in range(0,len(titles)):
+        plt.subplot(3,3,i+1),plt.imshow(images[i],'gray')
+        plt.title(titles[i])
+        plt.xticks([]),plt.yticks([])
+    plt.show()
+
+    """
+    return otsu_dilated
 
 
 def find_digits(img):
@@ -113,15 +134,16 @@ def find_digits(img):
     img2, cnts, hier = cv2.findContours(img.copy(),cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
     cnts = sorted(cnts, key = cv2.contourArea, reverse = True)[:10]
     # get digitCoords
+    # res = img.copy
+    res = cv2.cvtColor(img,cv2.COLOR_GRAY2RGB)
     digitCoords = []
     for c in cnts:
-        """ hull vs rect?
-        hull = cv2.convexHull(c)
-        temp = draw_rect(temp, hull)
-        """
+        # ll vs rect?
+        # hull = cv2.convexHull(c)
+        # temp = draw_rect(temp, hull)
         (x, y, w, h) = cv2.boundingRect(c)
         rect_coords = (x,y,w,h)
-        print("coords",rect_coords)
+        # print("coords",rect_coords)
 
         # if the contour is sufficiently large, it must be a digit
         if (w >= 15 or w >= 0 and w <=10) and (h >= 40 and h <= 50):
@@ -131,17 +153,18 @@ def find_digits(img):
             # the digit 1
             if (w <= 10):
                 rect_coords = (x-12,y-3,w+12,h+2)
-                res = cv2.rectangle(img,(x-10,y),(x+w,y+h),(0,255,0),2)
+                cv2.rectangle(res,(x-12,y-3),(x+w,y+h-1),(0,255,0),2)
             else:
-                res = cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
-            cv2.imshow("RECT TEST", res)
-            print("coords that passed",(x, y, w, h))
+                cv2.rectangle(res,(x,y),(x+w,y+h),(0,255,0),2)
+            
+            # print("coords that passed",(x, y, w, h))
             #"""
 
             # add digit coords to lsit
             # digitCoords.append(c)
             digitCoords.append(rect_coords)
 
+    cv2.imshow("RECT TEST", res)
     return digitCoords
 
 def identify_digits(img, digitCoords):
@@ -164,13 +187,13 @@ def identify_digits(img, digitCoords):
 
         # define the set of 7 segments
         segments = [
-            ((0, 0), (w, dH)),  # top
+            ((dW, 0), (w-dW, dH)),  # top
             ((0, 0), (dW, h // 2)), # top-left
             ((w - dW, 0), (w, h // 2)), # top-right
-            ((0, (h // 2) - dHC) , (w, (h // 2) + dHC)), # center
+            ((dW, (h // 2) - dHC) , (w-dW, (h // 2) + dHC)), # center
             ((0, h // 2), (dW, h)), # bottom-left
             ((w - dW, h // 2), (w, h)), # bottom-right
-            ((0, h - dH), (w, h))   # bottom
+            ((dW, h - dH), (w-dW, h))   # bottom
         ]
 
         on = [0] * len(segments)
@@ -212,22 +235,21 @@ def find_top_left_point(rect_coords):
     for p in rect_coords:
         ordered.append(tuple(p))
 
-    print("ordered:", ordered)
+    # print("ordered:", ordered)
 
     # sort ascending x
     ordered.sort(key=lambda tup: tup[0])
-    print("after sort:", ordered)
+    # print("after sort:", ordered)
     ordered = ordered[:2]
 
     # sort descending y
     ordered.sort(key=lambda tup: tup[1])
-    print("after sort:", ordered[0])
+    # print("after sort:", ordered[0])
 
     return ordered[0]
             
 
 def display_digits(orig_img, screenCoords, digitCoords, identified):
-    print("SCREEN COORDS POINTS:", screenCoords)
     top_left = find_top_left_point(screenCoords)
 
     for j, c in enumerate(digitCoords):
@@ -253,29 +275,75 @@ def process_digits_img(img):
     # img_scaled = cv2.resize(img,None,fx=ratio,fy=ratio,interpolation=cv2.INTER_LINEAR)
 
     # crop the screen
-    pts = find_screen(img)
-    print("poinrt",pts)
+    screen_pts = find_screen(img)
+    # print("poinrt",screen_pts)
 
     # transform image so it is easier to parse
-    warped = perspective.four_point_transform(img, pts)
-    processed_screen = transform_img(warped)
+    warped = perspective.four_point_transform(img, screen_pts)
+    processed_screen = binarize_img(warped)
 
     digitCoords = find_digits(processed_screen)
 
     # the result image with the identified numbers
-    res = img.copy()
+    output_img = img.copy()
 
     identifiedDigits = identify_digits(processed_screen, digitCoords)
-    print("answer:", identifiedDigits)
+    # print("answer:", identifiedDigits)
 
-    display_digits(res, pts, digitCoords, identifiedDigits)
-    cv2.imshow("THERE IT IS", res)
+    display_digits(output_img, screen_pts, digitCoords, identifiedDigits)
+    cv2.imshow("THERE IT IS", output_img)
     cv2.imshow("display", warped)
+
+    return identifiedDigits, output_img, screen_pts, processed_screen
 
 # main function
 ratio = 0.2
-img = cv2.imread("test3.jpg")
-img_scaled = cv2.resize(img,None,fx=ratio,fy=ratio,interpolation=cv2.INTER_LINEAR)
-process_digits_img(img_scaled)
+
+test_images = []
+res_images = []
+bin_images = []
+screen_pts = []
+solution = [(0,5,1,1)]
+
+# load test files
+img_dir = "C:/Users/gohna/Documents/bioe reu/opentrons/robobench/calibration/test_imgs" 
+if os.path.exists(img_dir):
+    os.chdir(img_dir)
+    for file in glob.glob("*.jpg"):
+        print(file)
+        test_images.append(img_dir+'/'+str(file))
+
+for j in range(0,len(test_images)):
+    img = cv2.imread(test_images[j])
+    img_scaled = cv2.resize(img,None,fx=ratio,fy=ratio,interpolation=cv2.INTER_LINEAR)
+    numbers, res, pts, binary_img = process_digits_img(img_scaled)
+    print("RESULT:",numbers)
+    res_images.append(res)
+    bin_images.append(binary_img)
+    screen_pts.append(find_top_left_point(pts))
+
+for i in range(0,len(test_images)):
+    pt = screen_pts[i]
+    x = pt[0]
+    y = pt[1]
+    w = 200
+    h = 100
+    crop_img = res_images[i][y-50:y+h, x-50:x+w] 
+    plt.subplot(3,3,i+1),plt.imshow(crop_img)
+    plt.title(test_images[i])
+    plt.xticks([]),plt.yticks([])
+plt.show()
+
+for i in range(0,len(test_images)):
+    pt = screen_pts[i]
+    x = pt[0]
+    y = pt[1]
+    w = 200
+    h = 100
+    plt.subplot(3,3,i+1),plt.imshow(bin_images[i])
+    plt.title("binary img"+str(i))
+    plt.xticks([]),plt.yticks([])
+plt.show()
+
 cv2.waitKey(0)
 cv2.destroyAllWindows()
