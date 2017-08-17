e916926f3bfa1b22abb7a2b97c8769f6615c4ed8
Anton Jackson-Smith <anton@dyingstar.net>
2017-08-17 01:37:57 -0700

    interface

--- robobench/frontend/frontend/src/App.js
+++ robobench/frontend/frontend/src/App.js
@@ -79,44 +79,44 @@ class Grid extends Component {
     })
 
     return (
-      <div id="grid-holder">
         <table id="grid">
-          <tr>
-            <th ></th>
-            <th>1</th>
-            <th>2</th>
-            <th>3</th>
-            <th>4</th>
-            <th>5</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr className="slot-grid">
-            <th scope="row">C</th>
-            <td>{grid['C1']}</td>
-            <td>{grid['C2']}</td>
-            <td>{grid['C3']}</td>
-            <td>{grid['C4']}</td>
-            <td>{grid['C5']}</td>
-          </tr>
-          <tr className="slot-grid">
-            <th scope="row">B</th>
-            <td>{grid['B1']}</td>
-            <td>{grid['B2']}</td>
-            <td>{grid['B3']}</td>
-            <td>{grid['B4']}</td>
-            <td>{grid['B5']}</td>
-          </tr>
-          <tr className="slot-grid">
-            <th scope="row">A</th>
-            <td>{grid['A1']}</td>
-            <td>{grid['A2']}</td>
-            <td>{grid['A3']}</td>
-            <td>{grid['A4']}</td>
-            <td>{grid['A5']}</td>
-          </tr>
+          <thead>
+            <tr>
+              <th></th>
+              <th>1</th>
+              <th>2</th>
+              <th>3</th>
+              <th>4</th>
+              <th>5</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="row">C</th>
+              <td>{grid['C1']}</td>
+              <td>{grid['C2']}</td>
+              <td>{grid['C3']}</td>
+              <td>{grid['C4']}</td>
+              <td>{grid['C5']}</td>
+            </tr>
+            <tr>
+              <th scope="row">B</th>
+              <td>{grid['B1']}</td>
+              <td>{grid['B2']}</td>
+              <td>{grid['B3']}</td>
+              <td>{grid['B4']}</td>
+              <td>{grid['B5']}</td>
+            </tr>
+            <tr>
+              <th scope="row">A</th>
+              <td>{grid['A1']}</td>
+              <td>{grid['A2']}</td>
+              <td>{grid['A3']}</td>
+              <td>{grid['A4']}</td>
+              <td>{grid['A5']}</td>
+            </tr>
+          </tbody>
         </table>
-      </div>
     )
   }
 }
@@ -124,58 +124,32 @@ class Grid extends Component {
 class App extends Component {
   render() {
     return (
-      <div>
-        <nav className="navbar navbar-expand-md navbar-dark sticky-top bg-dark">
-          <a className="navbar-brand" href="#">
-            <i className="fa fa-cubes" aria-hidden="true"></i> Dashboard
-          </a>
-          <button className="navbar-toggler d-lg-none" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-
-          <div className="collapse navbar-collapse" id="navbarsExampleDefault">
-            <ul className="navbar-nav mr-auto">
-              <li className="nav-item active">
-                <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">Settings</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">Help</a>
-              </li>
-            </ul>
-
-          </div>
-        </nav>
-
         <div className="row fill">
           {/* right window */}
           <div className="col-sm-2" id="dashboard">
-          <ul id="protocols">
-            <li><button type="button" className="btn">Dilution</button></li>
-            <li>
-              <button type="button" className="btn">Transfer</button>
+            <ul id="protocols">
+              <li><button type="button" className="btn">Dilution</button></li>
               <li>
-                  <div className="input-group">
-                  <input type="text" className="form-control" placeholder="Volume..." />
-                  <span className="input-group-btn">
-                    <button className="btn btn-default" type="button">Set</button>
-                  </span>
-                  </div>
-                </li>
-            </li>
-            <li><button type="button" className="btn btn-success"><i className="fa fa-play" aria-hidden="true"></i> Run </button></li>
-          </ul>
-        </div>
+                <button type="button" className="btn">Transfer</button>
+                <li>
+                    <div className="input-group">
+                    <input type="text" className="form-control" placeholder="Volume..." />
+                    <span className="input-group-btn">
+                      <button className="btn btn-default" type="button">Set</button>
+                    </span>
+                    </div>
+                  </li>
+              </li>
+              <li><button type="button" className="btn btn-success"><i className="fa fa-play" aria-hidden="true"></i> Run </button></li>
+            </ul>
+          </div>
 
 
         {/* deck grid */}
-          <div className="col-sm-10" id="grid">
+          <div className="col-sm-10 fill">
             <Grid />
           </div>
         </div>
-      </div>
     );
   }
 }
