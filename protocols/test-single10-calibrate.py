66dc2b4b0fc6b2f11db12c3b01b928391f8016fd
OT Da5id <endylabinstruments@gmail.com>
2018-01-26 13:41:01 -0800

    Modified the robots.sh script so that it no longer stings the names together

---
+++ protocols/test-single10-calibrate.py
@@ -0,0 +1,37 @@
+from opentrons import robot, containers, instruments
+
+p10_tiprack = containers.load('tiprack-10ul', 'D2')
+p10single_tiprack = containers.load('tiprack-10ul','E3')
+p200_tiprack = containers.load('tiprack-200ul', 'C3')
+plate1 = containers.load('96-flat', 'C1')
+plate2 = containers.load('96-flat', 'D1')
+ddH2O = containers.load('point', 'A1', 'ddH2O-trough')
+trash = containers.load('point', 'B2', 'holywastedplasticbatman')
+
+p10single = instruments.Pipette(
+    axis='a',
+    max_volume=10,
+    min_volume=0.5,
+    tip_racks=[p10single_tiprack],
+    trash_container=trash,
+    channels=1,
+    name='p10-8s'
+)
+
+p200 = instruments.Pipette(
+    axis='b',
+    max_volume=200,
+    min_volume=20,
+    tip_racks=[p200_tiprack],
+    trash_container=trash,
+    channels=1,
+    name='p200-1'
+)
+
+p10single.pick_up_tip()
+p10single.aspirate(9, ddH2O)
+p10single.dispense(plate1.wells('A1'))
+p10single.blow_out()
+p10single.touch_tip()
+p10single.drop_tip()
+
--- robots/robot.sh
+++ robots/robot.sh
@@ -10,4 +10,10 @@ ln -s $ROBOT_PATH/$1 "$OT_PATH"
 
 export APP_DATA_DIR=$ROBOT_PATH/$1
 export ROBOT_DEV=/dev/ttyACM-$1
+
+if [ -v OLD_PS1 ]; then
+	export PS1="$OLD_PS1"
+fi
+
+export OLD_PS1="$PS1"
 export PS1="[$1] $PS1"
