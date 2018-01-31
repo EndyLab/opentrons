96d5f4b24181ad6c157e35d29e41d0bec99ea439
OT Da5id <endylabinstruments@gmail.com>
2018-01-31 12:09:41 -0800

    use correct calibrations

--- protocols/robot.py
+++ protocols/robot.py
@@ -1,17 +1,35 @@
 #!/usr/bin/python3
 
-from opentrons import robot, containers, instruments
 import os
 import IPython
 import glob
+
+# Determine the robot device we should be using.
+if not 'ROBOT_DEV' in os.environ:
+    print("Please select a robot device")
+    devs = glob.glob("/dev/ttyACM-*")
+    for i, dev in enumerate(devs):
+        print("  {}: {}".format(i, dev))
+    robot_dev = input()
+    if robot_dev.isdigit():
+        robot_dev = devs[int(robot_dev)]
+
+    print()
+else:
+    robot_dev = os.environ['ROBOT_DEV']
+
+robot_name = robot_dev.replace("/dev/ttyACM-", "")
+os.environ['APP_DATA_DIR'] = "/home/koeng/opentrons/robots/" + robot_name
+
+from opentrons import robot, containers, instruments
 
 # Configure the robot
 # Set up your deck layout here.
 
 #  Layout:
 #    A     B       C      D      E
-#  3 p200          pcr    pcr    p10
-#  2               pcr    pcr    p10
+#  3 p200          master source p10
+#  2               dest   source p10
 #  1       reagt          trash  p10
 #
 
@@ -19,13 +37,11 @@ p10s_tipracks = [containers.load('tiprack-10ul', 'E3')]
 p10_tipracks = [containers.load('tiprack-10ul', 'E2')]
 p200_tipracks = [containers.load('tiprack-200ul', 'A3')]
 
-pcrs = [containers.load('96-PCR-tall', 'C2'),
-  containers.load('96-PCR-tall', 'C3'),
-  containers.load('96-PCR-tall', 'D2'),
-  containers.load('96-PCR-tall', 'D3')]
-
+source = [containers.load('96-flat', 'D2'),
+  containers.load('96-flat', 'D3')]
+dest = containers.load('96-PCR-flat', 'C2')
+master = containers.load('PCR-strip-tall', 'C3')
 reagents = containers.load('tube-rack-2ml', 'B1')
-
 trash = containers.load('point', 'D1', 'holywastedplasticbatman')
 
 p10s = instruments.Pipette(
@@ -61,19 +77,6 @@ p200 = instruments.Pipette(
 # Go higher between wells
 robot.arc_height=20
 
-# Determine the robot device we should be using.
-if not 'ROBOT_DEV' in os.environ:
-    print("Please select a robot device")
-    devs = glob.glob("/dev/ttyACM*")
-    for i, dev in enumerate(devs):
-        print("  {}: {}".format(i, dev))
-    robot_dev = input()
-    if robot_dev.isdigit():
-        robot_dev = devs[int(robot_dev)]
-
-    print()
-else:
-    robot_dev = os.environ['ROBOT_DEV']
 
 print("Connecting to robot {}".format(robot_dev))
 robot.connect(robot_dev)
