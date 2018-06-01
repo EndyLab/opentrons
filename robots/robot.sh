3e3cde8d3c88b084605d0ef0bf8f48525a89edc0
OT Da5id <endylabinstruments@gmail.com>
2018-06-01 16:32:39 -0700

    robot.sh makes backups and only links the latest robot into the app

--- robots/robot.sh
+++ robots/robot.sh
@@ -5,11 +5,27 @@ echo Switching to robot $1
 ROBOT_PATH=~/opentrons/robots
 OT_PATH=~/.config/OT\ One\ App\ 2/otone_data
 
+if [ -v APP_DATA_DIR ]; then
+	# We were already switched to a robot
+	# back it up
+	cp $APP_DATA_DIR/calibrations/calibrations.json $APP_DATA_DIR/calibrations/calibrations-`date -Iseconds`.json.bak
+fi
+
+for robotdev in `ls /dev/ttyACM-*`; do
+	if [ -h $robotdev ]; then
+		echo "Removing $robotdev"
+		sudo rm $robotdev
+	fi
+done
+
 rm "$OT_PATH"
 ln -s $ROBOT_PATH/$1 "$OT_PATH"
 
+export CURRENT_ROBOT=$1
 export APP_DATA_DIR=$ROBOT_PATH/$1
-export ROBOT_DEV=/dev/ttyACM-$1
+export ROBOT_DEV=/dev/$1
+
+sudo ln -s $ROBOT_DEV /dev/ttyACM-$1
 
 if [ -v OLD_PS1 ]; then
 	export PS1="$OLD_PS1"
